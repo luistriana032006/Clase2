c3569c096a391ad75cf8f3132a533b1b4b00fdf7
LuisMiguelTrianaRueda <luistriana617@gmail.com>
2024-10-31 17:41:02 -0500

    EjercicioClase2

---
+++ Ejercicio1/Ejercicio1.java
@@ -0,0 +1,16 @@
+package Ejercicio1;
+
+ public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        
+        System.out.println(" Desarrollar el siguiente ejercicio ");
+        System.out.println( " 6 / 2 ( 1 + 2 ) ");
+        
+        int resultado;
+
+         resultado = 6 / 2*( 1 + 2 );
+          
+         System.out.println( " El resultado es " + resultado);
+    }
+}
